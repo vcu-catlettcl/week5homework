e6a27fecb915e9b7072207fc55cc24a531c97ba8
vcu-catlettcl <78384776+vcu-catlettcl@users.noreply.github.com>
2021-03-08 21:41:59 -0500

    Attempt 6

--- fun/homework.py
+++ fun/homework.py
@@ -7,7 +7,7 @@ def find_greatest_number(incoming_list):
     Find the largest number in the list.
     """
     return max(incoming_list)
-    pass
+pass
 
 
 def find_least_number(incoming_list):
@@ -16,7 +16,7 @@ def find_least_number(incoming_list):
     Find the smallest/least number in the list.
     """
     return min(incoming_list)
-    pass
+pass
 
 
 def add_list_numbers(incoming_list):
@@ -25,8 +25,8 @@ def add_list_numbers(incoming_list):
     Add all the values together and return it.
     """
     incoming_list_sum = sum(incoming_list)
-    return incoming_list_sum
-    pass
+    print incoming_list_sum
+pass
 
 
 def longest_value_key(incoming_dict):
@@ -35,5 +35,5 @@ def longest_value_key(incoming_dict):
     Find the KEY that has a value with the highest length, use the len() function
     """
     longest_value_key_max = len(max(incoming_dict.keys))
-    return longest_value_key_max
-    pass
+    print longest_value_key_max
+pass
